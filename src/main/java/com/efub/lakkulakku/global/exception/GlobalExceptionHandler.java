8dada31042b5b4f57fae4c3cb1229bfa12246602
Minji Kang <68282057+nitronium102@users.noreply.github.com>
2022-07-16 16:04:18 +0900

    feat: 에러 핸들링 추가
    globalExceptionHandler와 에러 코드, 에러Response 양식을 지정했습니다.
    customException을 생성하고 에러 코드와 globalExceptionHandler에 해당 에러에 관한 정보를 추가하여 사용합니다.
    resolves #3

---
+++ src/main/java/com/efub/lakkulakku/global/exception/GlobalExceptionHandler.java
@@ -0,0 +1,35 @@
+package com.efub.lakkulakku.global.exception;
+
+import com.efub.lakkulakku.domain.diary.exception.DuplicateDiaryException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+	// Diary 관련 error handler
+	@ExceptionHandler(DuplicateDiaryException.class)
+	protected final ResponseEntity<ErrorResponse> handleDuplicateDiaryException(DuplicateDiaryException e){
+		final ErrorResponse response = ErrorResponse.builder()
+			.status(HttpStatus.BAD_REQUEST)
+			.code(ErrorCode.DUPLICATE_DIARY_DATE)
+			.message(e.getMessage())
+			.build();
+		return ResponseEntity.status(response.getStatus()).body(response);
+	}
+
+	// 기본적인 RunTimeException handler
+	@ExceptionHandler(RuntimeException.class)
+	protected final ResponseEntity<ErrorResponse> handleRunTimeException(RuntimeException e){
+		final ErrorResponse response = ErrorResponse.builder()
+			.status(HttpStatus.BAD_REQUEST)
+			.code(ErrorCode.DUPLICATE_DIARY_DATE)
+			.message(e.getMessage())
+			.build();
+		return ResponseEntity.status(response.getStatus()).body(response);
+	}
+}
