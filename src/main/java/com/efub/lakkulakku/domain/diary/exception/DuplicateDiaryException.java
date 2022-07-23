9a6ce29c845f0914c1f6ac97b784207276d90077
Minji Kang <68282057+nitronium102@users.noreply.github.com>
2022-07-23 23:12:33 +0900

    feat: Diary 관련 예외 추가
    #4

--- src/main/java/com/efub/lakkulakku/domain/diary/exception/DuplicateDiaryException.java
+++ src/main/java/com/efub/lakkulakku/domain/diary/exception/DuplicateDiaryException.java
@@ -1,5 +1,7 @@
 package com.efub.lakkulakku.domain.diary.exception;
 
 public class DuplicateDiaryException extends IllegalArgumentException {
-	public DuplicateDiaryException() {super("해당 날짜에 이미 다이어리가 존재합니다."); }
+	public DuplicateDiaryException() {
+		super("해당 날짜에 이미 다이어리가 존재합니다.");
+	}
 }
