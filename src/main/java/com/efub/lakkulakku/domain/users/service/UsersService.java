af12f4e77d3fa99606e349abae270ce058a91f92
Minji Kang <68282057+nitronium102@users.noreply.github.com>
2022-07-26 16:34:49 +0900

    feat: passwordEncoder 추가
    #30

--- src/main/java/com/efub/lakkulakku/domain/users/service/UsersService.java
+++ src/main/java/com/efub/lakkulakku/domain/users/service/UsersService.java
@@ -2,13 +2,13 @@ package com.efub.lakkulakku.domain.users.service;
 
 import com.efub.lakkulakku.domain.profile.ProfileRepository;
 import com.efub.lakkulakku.domain.profile.entity.Profile;
-import com.efub.lakkulakku.domain.user.dto.WithdrawReqDto;
 import com.efub.lakkulakku.domain.users.dto.LoginReqDto;
 import com.efub.lakkulakku.domain.users.dto.SignupReqDto;
 import com.efub.lakkulakku.domain.users.dto.WithdrawReqDto;
 import com.efub.lakkulakku.domain.users.entity.Users;
 import com.efub.lakkulakku.domain.users.repository.UsersRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -19,10 +19,16 @@ public class UsersService {
 
     private final UsersRepository usersRepository;
     private final ProfileRepository profileRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Transactional
     public Users signup(SignupReqDto reqDto) {
 //        return usersRepository.save(reqDto.toEntity());
+        //TODO : 중복체크
+        //TODO : 이메일, 비밀번호 형식 체크(필요하다면)
+        String encodedPassword = passwordEncoder.encode(reqDto.getPassword());
+        reqDto.setPassword(encodedPassword);
+
         Users user = usersRepository.save(reqDto.toEntity());
         Profile profile = Profile.builder()
                             .file(null)
