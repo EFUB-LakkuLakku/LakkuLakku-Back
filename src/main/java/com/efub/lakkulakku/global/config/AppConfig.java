e72abdd516105f7f89d3b960c6f946e1b34e684f
Minji Kang <68282057+nitronium102@users.noreply.github.com>
2022-07-27 10:49:02 +0900

    feat: filterChain 설정 변경
    - token 만료를 위한 exceptionHandling 추가
    - customFilter 위치 변경

--- src/main/java/com/efub/lakkulakku/global/config/AppConfig.java
+++ src/main/java/com/efub/lakkulakku/global/config/AppConfig.java
@@ -1,16 +1,26 @@
 package com.efub.lakkulakku.global.config;
 
+import com.efub.lakkulakku.global.config.jwt.JwtAuthenticationFilter;
+import com.efub.lakkulakku.global.config.jwt.JwtProvider;
+import com.efub.lakkulakku.global.exception.jwt.CustomAuthenticationEntryPoint;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class AppConfig {
+
+	private final JwtProvider jwtProvider;
+	private final CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
 
 	@Bean
 	public PasswordEncoder passwordEncoder() {
@@ -20,9 +30,16 @@ public class AppConfig {
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http.cors().and().csrf().disable()
+				.exceptionHandling()
+				.authenticationEntryPoint(customAuthenticationEntryPoint)
+				.and()
+				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+				.and()
 				.authorizeRequests()
 				.antMatchers("/api/v1/users/**").permitAll()
-				.anyRequest().authenticated();
+				.anyRequest().authenticated()
+				.and()
+				.addFilterBefore(new JwtAuthenticationFilter(jwtProvider), UsernamePasswordAuthenticationFilter.class);
 		return http.build();
 	}
 }
