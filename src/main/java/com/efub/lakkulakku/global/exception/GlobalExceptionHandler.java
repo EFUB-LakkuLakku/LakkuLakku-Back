7a5a3cdd76c9c9bb183c8dcdb718f077d674e80d
Minji Kang <68282057+nitronium102@users.noreply.github.com>
2022-07-29 20:01:13 +0900

    feat: Diary 에러별 코드 및 handler 추가

--- src/main/java/com/efub/lakkulakku/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/efub/lakkulakku/global/exception/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.efub.lakkulakku.global.exception;
 
+import com.efub.lakkulakku.domain.diary.exception.BadDateRequestException;
+import com.efub.lakkulakku.domain.diary.exception.DiaryNotFoundException;
 import com.efub.lakkulakku.domain.diary.exception.DuplicateDiaryException;
 import com.efub.lakkulakku.domain.file.exception.FileExtenstionException;
 import com.efub.lakkulakku.domain.file.exception.S3IOException;
@@ -16,32 +18,66 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.time.DateTimeException;
+
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-	// Diary 관련 error handler
-	@ExceptionHandler(DuplicateDiaryException.class)
-	protected final ResponseEntity<ErrorResponse> handleDuplicateDiaryException(DuplicateDiaryException e) {
-		final ErrorResponse response = ErrorResponse.builder()
-				.status(HttpStatus.BAD_REQUEST)
-				.code(ErrorCode.DUPLICATE_DIARY_DATE)
-				.message(e.getMessage())
-				.build();
-		return ResponseEntity.status(response.getStatus()).body(response);
-	}
-
-	// 기본적인 RunTimeException handler
+	/*================== Basic Exception ==================*/
 	@ExceptionHandler(RuntimeException.class)
 	protected final ResponseEntity<ErrorResponse> handleRunTimeException(RuntimeException e) {
 		final ErrorResponse response = ErrorResponse.builder()
 				.status(HttpStatus.BAD_REQUEST)
+				.code(ErrorCode.RUNTIME_EXCEPTION)
+				.message(e.getMessage())
+				.build();
+		return ResponseEntity.status(response.getStatus()).body(response);
+	}
+
+	@ExceptionHandler(DateTimeException.class)
+	protected final ResponseEntity<ErrorResponse> handleDateTimeException(DateTimeException e) {
+		final ErrorResponse response = ErrorResponse.builder()
+				.status(HttpStatus.BAD_REQUEST)
+				.code(ErrorCode.BAD_DATE_REQUEST)
+				.message(e.getMessage())
+				.build();
+		return ResponseEntity.status(response.getStatus()).body(response);
+	}
+
+	/*================== Diary Exception ==================*/
+	@ExceptionHandler(DuplicateDiaryException.class)
+	protected final ResponseEntity<ErrorResponse> handleDuplicateDiaryException(DuplicateDiaryException e) {
+		final ErrorResponse response = ErrorResponse.builder()
+				.status(HttpStatus.CONFLICT)
 				.code(ErrorCode.DUPLICATE_DIARY_DATE)
 				.message(e.getMessage())
 				.build();
 		return ResponseEntity.status(response.getStatus()).body(response);
 	}
 
+	@ExceptionHandler(DiaryNotFoundException.class)
+	protected final ResponseEntity<ErrorResponse> handleDiaryNotFoundException(DiaryNotFoundException e) {
+		final ErrorResponse response = ErrorResponse.builder()
+				.status(HttpStatus.NOT_FOUND)
+				.code(ErrorCode.DIARY_NOT_FOUND)
+				.message(e.getMessage())
+				.build();
+		return ResponseEntity.status(response.getStatus()).body(response);
+	}
+
+	@ExceptionHandler(BadDateRequestException.class)
+	protected final ResponseEntity<ErrorResponse> handleBadDateRequestException(BadDateRequestException e) {
+		final ErrorResponse response = ErrorResponse.builder()
+				.status(HttpStatus.BAD_REQUEST)
+				.code(ErrorCode.BAD_DATE_REQUEST)
+				.message(e.getMessage())
+				.build();
+		return ResponseEntity.status(response.getStatus()).body(response);
+	}
+
+
+	/*================== User Exception ==================*/
 	@ExceptionHandler(DuplicateEmailException.class)
 	protected final ResponseEntity<ErrorResponse> handleDuplicateEmailException(DuplicateEmailException e) {
 		final ErrorResponse response = ErrorResponse.builder()
@@ -62,16 +98,6 @@ public class GlobalExceptionHandler {
 		return ResponseEntity.status(response.getStatus()).body(response);
 	}
 
-	@ExceptionHandler(DuplicateFriendException.class)
-	protected final ResponseEntity<ErrorResponse> handleDuplicateFriendException(DuplicateFriendException e) {
-		final ErrorResponse response = ErrorResponse.builder()
-				.status(HttpStatus.BAD_REQUEST)
-				.code(ErrorCode.DUPLICATE_FRIEND)
-				.message(e.getMessage())
-				.build();
-		return ResponseEntity.status(response.getStatus()).body(response);
-	}
-
 	// 존재하지 않는 유저
 	@ExceptionHandler(UserNotFoundException.class)
 	protected final ResponseEntity<ErrorResponse> handleUserNotFoundException(UserNotFoundException e) {
@@ -83,6 +109,18 @@ public class GlobalExceptionHandler {
 		return ResponseEntity.status(response.getStatus()).body(response);
 	}
 
+	/*================== Friend Exception ==================*/
+	@ExceptionHandler(DuplicateFriendException.class)
+	protected final ResponseEntity<ErrorResponse> handleDuplicateFriendException(DuplicateFriendException e) {
+		final ErrorResponse response = ErrorResponse.builder()
+				.status(HttpStatus.BAD_REQUEST)
+				.code(ErrorCode.DUPLICATE_FRIEND)
+				.message(e.getMessage())
+				.build();
+		return ResponseEntity.status(response.getStatus()).body(response);
+	}
+
+	/*================== File Exception ==================*/
 	// io 예외
 	@ExceptionHandler(S3IOException.class)
 	protected final ResponseEntity<ErrorResponse> handleS3IOException(S3IOException e) {
@@ -105,7 +143,7 @@ public class GlobalExceptionHandler {
 		return ResponseEntity.status(response.getStatus()).body(response);
 	}
 
-	// Security
+	/*================== Security Exception ==================*/
 	@ExceptionHandler(BadTokenRequestException.class)
 	protected final ResponseEntity<ErrorResponse> handleBadTokenRequestException(BadTokenRequestException e) {
 		final ErrorResponse response = ErrorResponse.builder()
