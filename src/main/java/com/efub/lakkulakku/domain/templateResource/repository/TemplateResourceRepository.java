315323efd49f59fd3cbc29f4b65d57dd9028de24
Minji Kang <68282057+nitronium102@users.noreply.github.com>
2022-08-02 05:11:39 +0900

    feat: Template 조회 관련 로직 추가
    #35

--- src/main/java/com/efub/lakkulakku/domain/templateResource/repository/TemplateResourceRepository.java
+++ src/main/java/com/efub/lakkulakku/domain/templateResource/repository/TemplateResourceRepository.java
@@ -3,5 +3,8 @@ package com.efub.lakkulakku.domain.templateResource.repository;
 import com.efub.lakkulakku.domain.templateResource.entity.TemplateResource;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TemplateResourceRepository extends JpaRepository<TemplateResource, Long> {
+	List<TemplateResource> findByCategory(String category);
 }
