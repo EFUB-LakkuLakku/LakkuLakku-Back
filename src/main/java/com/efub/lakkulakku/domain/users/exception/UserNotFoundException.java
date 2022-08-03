72d22cb190d4e3a691c1f747b26b7b0abf081402
Minji Kang <68282057+nitronium102@users.noreply.github.com>
2022-08-03 16:27:11 +0900

    feat: UserNotFoundException exception 유형 수정

--- src/main/java/com/efub/lakkulakku/domain/users/exception/UserNotFoundException.java
+++ src/main/java/com/efub/lakkulakku/domain/users/exception/UserNotFoundException.java
@@ -1,8 +1,10 @@
 package com.efub.lakkulakku.domain.users.exception;
 
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
+
 import static com.efub.lakkulakku.global.constant.ResponseConstant.NOTFOUND_USER;
 
-public class UserNotFoundException extends IllegalArgumentException {
+public class UserNotFoundException extends ResourceNotFoundException {
 
 	public UserNotFoundException() {
 		super(NOTFOUND_USER);
