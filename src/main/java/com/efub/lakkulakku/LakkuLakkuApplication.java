fff081faf9412a4739af612d550f3f1f33dbd542
julia-ing <ywchoi0625@gmail.com>
2022-08-04 22:34:25 +0900

    fix: comment unauthorized exception id로 비교

--- src/main/java/com/efub/lakkulakku/LakkuLakkuApplication.java
+++ src/main/java/com/efub/lakkulakku/LakkuLakkuApplication.java
@@ -4,17 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-import javax.annotation.PostConstruct;
-import java.util.TimeZone;
-
 @EnableJpaAuditing
 @SpringBootApplication
 public class LakkuLakkuApplication {
-
-	@PostConstruct
-	public void start() {
-		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(LakkuLakkuApplication.class, args);
