ed3e86a7796d3f5ac1cf333ca672463f073d2324
june0216 <ziyun1612@ewhain.net>
2022-08-07 00:17:03 +0900

    fix : 홈 API 변경 - 프론트 요청으로 GET 방식으로 변경

--- src/main/java/com/efub/lakkulakku/domain/users/controller/HomeController.java
+++ src/main/java/com/efub/lakkulakku/domain/users/controller/HomeController.java
@@ -1,6 +1,6 @@
 package com.efub.lakkulakku.domain.users.controller;
 
-import com.efub.lakkulakku.domain.users.dto.UserGetReqDto;
+
 import com.efub.lakkulakku.domain.users.dto.HomeResDto;
 import com.efub.lakkulakku.domain.users.entity.Users;
 import com.efub.lakkulakku.domain.users.exception.UserNotFoundException;
@@ -10,7 +10,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
 
 @RestController
 @RequestMapping({"/api/v1/home/{year}/{month}", "/api/v1/home"})
@@ -20,12 +19,12 @@ public class HomeController {
 	private final UsersRepository usersRepository;
 	private final UsersService usersService;
 
-	@PostMapping
-	public ResponseEntity<HomeResDto> getHomeByDate(@Valid @RequestBody UserGetReqDto userGetReq, @PathVariable(value = "year", required = false) String year, @PathVariable(value = "month", required = false) String month) {
+	@GetMapping
+	public ResponseEntity<HomeResDto> getHomeByDate(@PathVariable(value = "year", required = false) String year, @PathVariable(value = "month", required = false) String month, @RequestParam String nickname) {
 		//TODO : LocalDateTime의 범위를 넘어가는 경우 에러 발생
 //		if (Integer.parseInt(year) <= 1969 || Integer.parseInt(year) >= 2100)
 //			throw new BadDateRequestException();
-		Users user = usersRepository.findByNickname(userGetReq.getNickname())
+		Users user = usersRepository.findByNickname(nickname)
 				.orElseThrow(() -> new UserNotFoundException());
 
 		return ResponseEntity.ok()
