76688d699db1f458f91aa84b3e24581c748feaf4
Minji Kang <68282057+nitronium102@users.noreply.github.com>
2022-10-09 17:08:00 +0900

    feat: [/home] 전체 알림 목록 조회 api 추가

--- src/main/java/com/efub/lakkulakku/domain/users/controller/HomeController.java
+++ src/main/java/com/efub/lakkulakku/domain/users/controller/HomeController.java
@@ -2,12 +2,13 @@ package com.efub.lakkulakku.domain.users.controller;
 
 
 import com.efub.lakkulakku.domain.diary.dto.DiaryHomeResDto;
-import com.efub.lakkulakku.domain.notification.dto.NotificationHomeResDto;
+import com.efub.lakkulakku.domain.notification.dto.NotificationResDto;
 import com.efub.lakkulakku.domain.users.dto.ProfileUpdateResDto;
 import com.efub.lakkulakku.domain.users.dto.UserGetReqDto;
 import com.efub.lakkulakku.domain.users.entity.Users;
 import com.efub.lakkulakku.domain.users.exception.UserNotFoundException;
 import com.efub.lakkulakku.domain.users.repository.UsersRepository;
+import com.efub.lakkulakku.domain.users.service.AuthUsers;
 import com.efub.lakkulakku.domain.users.service.UsersService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -31,7 +32,7 @@ public class HomeController {
 //		if (Integer.parseInt(year) <= 1969 || Integer.parseInt(year) >= 2100)
 //			throw new BadDateRequestException();
 		Users user = usersRepository.findByNickname(reqDto.getNickname())
-				.orElseThrow(() -> new UserNotFoundException());
+				.orElseThrow(UserNotFoundException::new);
 
 		return usersService.getHomeDiary(user, year, month);
 	}
@@ -39,17 +40,14 @@ public class HomeController {
 	@GetMapping("/user")
 	public ResponseEntity<ProfileUpdateResDto> getHomeUser(@Valid @RequestBody UserGetReqDto reqDto) {
 		Users user = usersRepository.findByNickname(reqDto.getNickname())
-				.orElseThrow(() -> new UserNotFoundException());
+				.orElseThrow(UserNotFoundException::new);
 
 		return ResponseEntity.ok()
 				.body(new ProfileUpdateResDto(user));
 	}
 
-	@GetMapping("/alarm")
-	public List<NotificationHomeResDto> getHomeAlarm(@Valid @RequestBody UserGetReqDto reqDto) {
-		Users user = usersRepository.findByNickname(reqDto.getNickname())
-				.orElseThrow(() -> new UserNotFoundException());
-
-		return usersService.getHomeAlarm(user);
+	@GetMapping("/notification")
+	public List<NotificationResDto> getHomeNotification(@AuthUsers Users user) {
+		return usersService.findAllNotifications(user);
 	}
 }
--- src/main/java/com/efub/lakkulakku/domain/users/dto/HomeMapper.java
+++ src/main/java/com/efub/lakkulakku/domain/users/dto/HomeMapper.java
@@ -2,7 +2,7 @@ package com.efub.lakkulakku.domain.users.dto;
 
 import com.efub.lakkulakku.domain.diary.dto.DiaryHomeMapper;
 import com.efub.lakkulakku.domain.diary.repository.DiaryRepository;
-import com.efub.lakkulakku.domain.notification.dto.NotificationHomeMapper;
+import com.efub.lakkulakku.domain.notification.dto.NotificationMapper;
 import com.efub.lakkulakku.domain.notification.repository.NotificationRepository;
 import com.efub.lakkulakku.domain.users.entity.Users;
 import lombok.AccessLevel;
@@ -17,7 +17,6 @@ public class HomeMapper {
 
 	private final DiaryHomeMapper diaryHomeMapper;
 	private final DiaryRepository diaryRepository;
-	private final NotificationHomeMapper notificationHomeMapper;
 	private final NotificationRepository notificationRepository;
 
 	public HomeResDto toHomeResDto(Users entity, String year, String month) {
@@ -27,7 +26,8 @@ public class HomeMapper {
 		return HomeResDto.builder()
 				.user(new ProfileUpdateResDto(entity))
 				.diary(diaryRepository.findUsersDiaryByYearAndMonth(entity.getId(), year, month).stream().map(diaryHomeMapper::toDiaryHomeResDto).collect(Collectors.toList()))
-				.alarm(notificationRepository.findByUsersId(entity).stream().map(notificationHomeMapper::toNotificationHomeResDto).collect(Collectors.toList()))
+				.alarm(notificationRepository.findByUsersId(entity.getId()).stream().map(NotificationMapper::toNotificationResDto).collect(Collectors.toList()))
 				.build();
+
 	}
 }
