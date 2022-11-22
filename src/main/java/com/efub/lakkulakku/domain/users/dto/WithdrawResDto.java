5a12cb19e2cff93b8cad7dbb1c8237d5363b70a3
Minji Kang <68282057+nitronium102@users.noreply.github.com>
2022-11-22 11:48:25 +0900

    feat: 회원탈퇴 api 및 res dto 추가

---
+++ src/main/java/com/efub/lakkulakku/domain/users/dto/WithdrawResDto.java
@@ -0,0 +1,19 @@
+package com.efub.lakkulakku.domain.users.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class WithdrawResDto {
+
+	private String nickname;
+	private String message;
+
+	@Builder
+	public WithdrawResDto(String nickname, String message){
+		this.nickname = nickname;
+		this.message = message;
+	}
+}
