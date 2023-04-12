cabfc34a7cbc679c075c88c3d89af6a54eb2a1fd
june0216 <ziyun1612@ewhain.net>
2023-04-12 14:51:01 +0900

    feat : LoginController, SignUpController 테스트 코드 작성 - login의 경우 상태 메시지가 created로 되어있던 것을 ok로 변환

--- src/test/java/com/efub/lakkulakku/domain/users/controller/LoginControllerTest.java
+++ src/test/java/com/efub/lakkulakku/domain/users/controller/LoginControllerTest.java
@@ -1,31 +1,24 @@
 package com.efub.lakkulakku.domain.users.controller;
 
+import com.efub.lakkulakku.domain.users.dto.LoginInfoDto;
+import com.efub.lakkulakku.domain.users.dto.LoginReqDto;
+import com.efub.lakkulakku.domain.users.dto.LoginResDto;
 import com.efub.lakkulakku.domain.users.dto.SignupReqDto;
 import com.efub.lakkulakku.domain.users.entity.Users;
 import com.efub.lakkulakku.domain.users.service.UsersService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.test.web.servlet.MockMvc;
 
-import java.util.Optional;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
-@ExtendWith(MockitoExtension.class) //Junit4의 Runwith과 같은 기능을 하는 Junit5 어노테이션
-class LoginControllerTest {
+public class LoginControllerTest {
 
 	@InjectMocks
 	LoginController loginController;
@@ -34,39 +27,37 @@ class LoginControllerTest {
 	UsersService usersService;
 
 	@Test
-	@DisplayName("회원가입_서비스_성공")
-	void signupController()  {
+	@DisplayName("로그인_서비스_성공")
+	void loginUsersService(){
 		String email = "test1234@gmail.com";
 		String password = "test1234!!";
 		String nickname = "test";
-		String bio = "반갑습니다 :)";
-		final SignupReqDto reqDto = SignupReqDto.builder()
+
+		String accessToken = "accessToken";
+		String refreshToken = "refreshToken";
+		final LoginReqDto reqDto = LoginReqDto.builder()
 				.email(email)
 				.password(password)
-				.nickname(nickname)
 				.build();
 
-		Users users = reqDto.toEntity();
+		final LoginInfoDto loginInfoDto = LoginInfoDto.builder()
+				.refreshToken(refreshToken)
+				.accessToken(accessToken)
+				.nickname(nickname)
+				.build();
+		LoginResDto loginResDto = loginInfoDto.toLoginResDto();
+		given(usersService.login(email, password)).willReturn(loginInfoDto);
 
-		given(usersService.signup(any(SignupReqDto.class))).willReturn(users);
 
 		// when
-		ResponseEntity<String> response = loginController.signup(reqDto);
+		ResponseEntity<LoginResDto> response = loginController.login(reqDto);
 
 		// then
-		verify(usersService).signup(reqDto);//usersService.signup() 메소드가 호출되었는지 확인
+		verify(usersService).login(email, password);//usersService.signup() 메소드가 호출되었는지 확인
 		assertEquals(HttpStatus.OK, response.getStatusCode());
-		assertEquals("성공적으로 가입되었습니다.", response.getBody());
+		//assertEquals("성공적으로 가입되었습니다.", response.getBody());
 
 	}
 
-	@Test
-	@DisplayName("로그인_서비스_성공")
-	void loginUsersService(){
-
 	}
 
-
-
-
-}
